a6c212ac9a9068d6ae8c454c0d743305c1e458d4
Velyo Danchev <velyo.t.danchev@gmail.com>
2026-05-03 14:04:33 +0300

    homework-velyo-danchev-06

---
+++ homework/velyo-danchev/homework-6.spec.ts
@@ -0,0 +1,74 @@
+import {test, expect} from '@playwright/test'
+
+// create the below 2 test scenarios and put them in a test suite
+// put all groups of actions into a test.step
+// use soft assertions, unless we are assrting a navigation has taken place
+// try to add meaningful comments to all assertions
+// you can find test execution command examples in the Readme.md file in project's main folder
+// you can add -ui mode debug your tests during execution
+// after execution view html report with command: 'npx playwright show-report' (if any tests failed report will open automaticaly)
+// a trace artifact will be added to the report if any test has failed, you can use as well to debug your tests
+
+
+// first test scenario use CSS and XPATH selectors like page.locator('location string'):
+
+// navigate to https://rahulshettyacademy.com/AutomationPractice/
+// assert succesful naviation
+
+// check radio button #2
+// assert Radio button #2 is checked
+
+// fill the input with "Bulgaria"
+// click on the first auto complete suggestion
+// Verify the input value is "Bulgaria"
+
+// select option #2 from the dropdown
+// Verify the selected option is "option2"
+
+// check checkbox #3
+// Verify checkbox #3 is checked
+
+// click on the "Hide" button
+// Verify the input element is now hidden
+
+// hover over the "Mouse Hover" element
+// Verify the "Top" link is now visible
+
+test('example using css/xpath  as locators', async({page}) => {
+    await page.goto(`https://rahulshettyacademy.com/AutomationPractice/`);
+    await expect(page).toHaveURL(/AutomationPractice/);
+    await page.locator('input[value="radio2"]').check();
+    await expect(page.locator('input[value="radio2"]')).toBeChecked();
+    await page.locator('#autocomplete').fill('Bulgaria');
+    await page.locator('li.ui-menu-item:has-text("Bulgaria")').click();
+    await expect(page.locator('#autocomplete')).toHaveValue('Bulgaria');
+    await page.locator('input[value="radio3"]').check();
+    await expect(page.locator('input[value="radio3"]')).toBeChecked();
+    await page.locator('#hide-textbox').click();
+    await expect(page.locator('#displayed-text')).toBeHidden();
+    await page.locator('#mousehover').hover();
+    await expect(page.locator('a[href="#top"]')).toBeVisible();
+} )
+
+
+// second test scenario use only Playwright selectors like page.getByRole('button', { name: 'Вход' }).click();
+
+// navigate to https://testautomationpractice.blogspot.com/p/playwrightpractice.html
+// assert succesful naviation
+
+// In Playwright Practice session group 1. getByRole() Locators pick a locator from section Navigation and click on it.
+// assert navigation has taken place
+
+// In Playwright Practice session group 2. getByText() Locators pick one of the paragraphs with colored text and assert its color
+
+// In Playwright Practice session group 3. getByLabel() Locators Locators check the unchecked radio button
+// assert is is checked now
+
+// In Playwright Practice session group 4. getByPlaceholder() Locators pick an input fill and assert it is editable
+// fill the input filed with text
+
+// In Playwright Practice session group 5.getByAltText() Locators assert the image is visible
+
+// In Playwright Practice session group 6. getByTitle() Locators assert one of the elements' full text
+
+// In Playwright Practice session group 7. getByTestId() Locators assert one of the element contains part of its text
